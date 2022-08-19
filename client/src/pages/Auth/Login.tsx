9c2abdaa0e2bca514ef4e6c0fecb21851eaabaff
pino._.hoo <inhoo23@naver.com>
2022-08-19 14:40:35 +0900

    SETTING

---
+++ client/src/pages/Auth/Login.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <input placeholder="Enter ID" />
+      <input placeholder="Enter PW" />
+      <button>LOGIN</button>
+      <button>Register</button>
+    </div>
+  )
+}
